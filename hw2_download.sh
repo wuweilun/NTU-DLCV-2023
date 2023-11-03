ea902172f6f736841ab0e9d1c624a40216fa195a
wuweilun <waynewuweilun@gmail.com>
2023-11-04 04:04:18 +0800

    add: model checkpoint &test bash file

---
+++ hw2_download.sh
@@ -0,0 +1,3 @@
+wget 'https://www.dropbox.com/scl/fi/1mgxv6c81531zr0p6esm1/hw2_model_checkpoint.zip?rlkey=47noa72wtwiubfki0mxcw5oci&dl=1' -O hw2_model_checkpoint.zip
+
+unzip ./hw2_model_checkpoint.zip 
