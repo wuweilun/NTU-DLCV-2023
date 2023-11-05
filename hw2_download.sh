e02d0d5da596c80d5d23808c02723d6b0870be77
wuweilun <waynewuweilun@gmail.com>
2023-11-06 03:51:17 +0800

    fix: checkpoint path

--- hw2_download.sh
+++ hw2_download.sh
@@ -1,3 +1,3 @@
-wget 'https://www.dropbox.com/scl/fi/1mgxv6c81531zr0p6esm1/hw2_model_checkpoint.zip?rlkey=47noa72wtwiubfki0mxcw5oci&dl=1' -O hw2_model_checkpoint.zip
+wget -O hw2_model_checkpoint.zip 'https://www.dropbox.com/scl/fi/1mgxv6c81531zr0p6esm1/hw2_model_checkpoint.zip?rlkey=47noa72wtwiubfki0mxcw5oci&dl=1'
 
 unzip ./hw2_model_checkpoint.zip 
