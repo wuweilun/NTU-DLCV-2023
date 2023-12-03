61658c0050ed704be2d006c6e4e7dd22081a9f9a
wuweilun <waynewuweilun@gmail.com>
2023-12-03 15:55:59 +0800

    add: validation, test code

---
+++ val.py
@@ -0,0 +1,121 @@
+import torch
+import os
+import numpy as np
+from collections import defaultdict
+from tqdm import tqdm
+import imageio
+from argparse import ArgumentParser
+
+from models.rendering import render_rays
+from models.nerf import *
+
+from utils import load_ckpt
+import metrics
+
+from dataset import KlevrDataset
+
+torch.backends.cudnn.benchmark = True
+
+def get_opts():
+    parser = ArgumentParser()
+    parser.add_argument('--root_dir', type=str,
+                        default='./dataset/',
+                        help='root directory of dataset')
+    parser.add_argument('--folder_output', type=str, default='val',
+                        help='output folder name')
+    parser.add_argument('--split', type=str, default='val',
+                        help='test or test_train')
+    parser.add_argument('--img_wh', nargs="+", type=int, default=[256, 256],
+                        help='resolution (img_w, img_h) of the image')
+    parser.add_argument('--spheric_poses', default=False, action="store_true",
+                        help='whether images are taken in spheric poses (for llff)')
+
+    parser.add_argument('--N_samples', type=int, default=64,
+                        help='number of coarse samples')
+    parser.add_argument('--N_importance', type=int, default=128,
+                        help='number of additional fine samples')
+    parser.add_argument('--use_disp', default=False, action="store_true",
+                        help='use disparity depth sampling')
+    parser.add_argument('--chunk', type=int, default=32*1024*4,
+                        help='chunk size to split the input to avoid OOM')
+
+    parser.add_argument('--ckpt_path', type=str, required=True,
+                        help='pretrained checkpoint path to load')
+
+    parser.add_argument('--save_depth', default=False, action="store_true",
+                        help='whether to save depth prediction')
+    parser.add_argument('--depth_format', type=str, default='pfm',
+                        choices=['pfm', 'bytes'],
+                        help='which format to save')
+
+    return parser.parse_args()
+
+
+@torch.no_grad()
+def batched_inference(models, embeddings,
+                      rays, N_samples, N_importance, use_disp,
+                      chunk):
+    """Do batched inference on rays using chunk."""
+    B = rays.shape[0]
+    chunk = 1024*32
+    results = defaultdict(list)
+    for i in range(0, B, chunk):
+        rendered_ray_chunks = \
+            render_rays(models,
+                        embeddings,
+                        rays[i:i+chunk],
+                        N_samples,
+                        use_disp,
+                        0,
+                        0,
+                        N_importance,
+                        chunk,
+                        test_time=True)
+
+        for k, v in rendered_ray_chunks.items():
+            results[k] += [v]
+
+    for k, v in results.items():
+        results[k] = torch.cat(v, 0)
+    return results
+
+
+if __name__ == "__main__":
+    args = get_opts()
+    w, h = args.img_wh
+    dir_name = args.folder_output
+    
+    kwargs = {'root_dir': args.root_dir,}
+              #'split': args.split,
+              #'img_wh': tuple(args.img_wh)}
+
+    dataset = KlevrDataset
+    dataset = dataset(split='val', **kwargs)
+    
+    embedding_xyz = Embedding(3, 10)
+    embedding_dir = Embedding(3, 4)
+    nerf_coarse = NeRF()
+    nerf_fine = NeRF()
+    load_ckpt(nerf_coarse, args.ckpt_path, model_name='nerf_coarse')
+    load_ckpt(nerf_fine, args.ckpt_path, model_name='nerf_fine')
+    nerf_coarse.cuda().eval()
+    nerf_fine.cuda().eval()
+
+    models = [nerf_coarse, nerf_fine]
+    embeddings = [embedding_xyz, embedding_dir]
+
+    imgs = []
+
+    for i in tqdm(range(len(dataset))):
+        sample = dataset[i]
+        rays = sample['rays'].cuda()
+        results = batched_inference(models, embeddings, rays,
+                                    args.N_samples, args.N_importance, args.use_disp,
+                                    args.chunk)
+
+        img_pred = results['rgb_fine'].view(h, w, 3).cpu().numpy()
+        img_pred_ = (img_pred*255).astype(np.uint8)
+        imgs += [img_pred_]
+        name = sample['name']
+        imageio.imwrite(os.path.join(dir_name, f'{name:05d}.png'), img_pred_)
+    
