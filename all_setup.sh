bc2871723338648ae1665b57ba51d12213f77133
jiangjiangzh <evatiffany642021@gmail.com>
2023-12-28 21:42:35 +0800

    Fix: remove VALOR and conda create from all_setup.sh

--- all_setup.sh
+++ all_setup.sh
@@ -1,6 +1,3 @@
-conda create -n flipped-vqa python=3.8
-conda activate flipped-vqa
-
 #setup.sh
 pip install torch==1.10.0+cu111 torchvision==0.11.0+cu111 torchaudio==0.10.0 -f https://download.pytorch.org/whl/torch_stable.html
 pip install fairscale
@@ -13,16 +10,6 @@ pip install setuptools==59.5.0
 pip install pysrt
 
 pip install gdown
-pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./VALOR/apex
-
-#preinstall.sh
-pip install tensorboardx
-pip install toolz
-pip install ipdb
-pip install matplotlib
-pip install pytorch_pretrained_bert
-pip install easydict
-pip install pyyaml
 
 gdown 1LeMob-x5z2zKMadL4BRJWjBt7dJ-Bh7d -O data.zip
 unzip ./data.zip
